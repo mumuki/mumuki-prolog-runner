b6a2a83e91c2a1a4bf30fc55342db621b4ba9c3e
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2014-12-31 15:44:24 -0300

    Handling exceptions

--- main.rb
+++ main.rb
@@ -8,17 +8,21 @@ config = YAML.load_file('config/application.yml')
 compiler = Plunit::TestCompiler.new
 runner = Plunit::TestRunner.new(config['test_runner_command'])
 
-post '/test' do
+def parse_test_body(request)
   compilation = JSON.parse request.body.read
-  test = compilation['test']
-  content = compilation['content']
+  [compilation['test'], compilation['content']]
+end
 
-  file = compiler.create_compilation_file!(test, content)
-  results = runner.run_test_file!(file)
-
-  file.unlink
-
-  JSON.generate(exit: results[0], out: results[1])
+post '/test' do
+  begin
+    test, content = parse_test_body request
+    file = compiler.create_compilation_file!(test, content)
+    results = runner.run_test_file!(file)
+    file.unlink
+    JSON.generate(exit: results[1], out: results[0])
+  rescue Exception => e
+    JSON.generate(exit: 'failed', out: e.message)
+  end
 end
 
 get '/*' do
