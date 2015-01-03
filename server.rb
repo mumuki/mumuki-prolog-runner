d694d4bb054f36f138b0f60f0a9b6a5ad82a54c2
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-01-02 21:01:30 -0300

    Moving to modular style

--- server.rb
+++ server.rb
@@ -1,39 +1,42 @@
-require 'sinatra'
+require 'sinatra/base'
 require 'yaml'
 require 'json'
 
 require_relative './lib/test_compiler'
 require_relative './lib/test_runner'
 
-configure :production do
-  set :config_filename, 'config/development.yml'
-end
-configure :production do
-  set :config_filename, 'config/production.yml'
-end
-
-config = YAML.load_file(settings.config_filename)
-
-compiler = TestCompiler.new
-runner = TestRunner.new(config['test_runner_command'])
-
-def parse_test_body(request)
-  compilation = JSON.parse request.body.read
-  [compilation['test'], compilation['content']]
-end
-
-post '/test' do
-  begin
-    test, content = parse_test_body request
-    file = compiler.create_compilation_file!(test, content)
-    results = runner.run_test_file!(file)
-    file.unlink
-    JSON.generate(exit: results[1], out: results[0])
-  rescue Exception => e
-    JSON.generate(exit: 'failed', out: e.message)
+class PlunitApp < Sinatra::Base
+  configure :production do
+    set :config_filename, 'config/development.yml'
   end
-end
+  configure :production do
+    set :config_filename, 'config/production.yml'
+  end
 
-get '/*' do
-  redirect config['mumuki_url']
+  config = YAML.load_file(settings.config_filename)
+  compiler = TestCompiler.new
+  runner = TestRunner.new(config['test_runner_command'])
+
+  helpers do
+    def parse_test_body(request)
+      compilation = JSON.parse request.body.read
+      [compilation['test'], compilation['content']]
+    end
+  end
+
+  post '/test' do
+    begin
+      test, content = parse_test_body request
+      file = compiler.create_compilation_file!(test, content)
+      results = runner.run_test_file!(file)
+      file.unlink
+      JSON.generate(exit: results[1], out: results[0])
+    rescue Exception => e
+      JSON.generate(exit: 'failed', out: e.message)
+    end
+  end
+
+  get '/*' do
+    redirect config['mumuki_url']
+  end
 end
