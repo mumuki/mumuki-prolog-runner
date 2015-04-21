ea30eecfb49d9f6176cb7da9785b57931638178e
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-04-21 20:53:19 -0300

    Implementing term generation

--- lib/expectations_runner.rb
+++ lib/expectations_runner.rb
@@ -1,7 +1,35 @@
+require 'json'
+require 'mumukit/inspection'
+
+class Mumukit::Inspection::PlainInspection
+  def to_term
+    "inspection('#{type}')"
+  end
+end
+
+class Mumukit::Inspection::TargetedInspection
+  def to_term
+    "inspection('#{type}','#{target}')"
+  end
+end
+
+class Mumukit::Inspection::NegatedInspection
+  def to_term
+    "not(#{@inspection.to_term})"
+  end
+end
+
 class ExpectationsRunner
+  include Mumukit
 
-  def expectations_to_terms(expectations)
 
+  def self.expectations_to_terms(expectations)
+    '[' + expectations.map do |e|
+      "expectation('#{e['binding']}',#{inspection_to_term(e['inspection'])})"
+    end.join('') + ']'
   end
 
+  def self.inspection_to_term(s)
+    Inspection.parse(s).to_term
+  end
 end
--- spec/expectations_runner_spec.rb
+++ spec/expectations_runner_spec.rb
@@ -1,23 +1,17 @@
 require_relative '../lib/expectations_runner'
 
 describe ExpectationsRunner do
-  let(:json) {
-    {expectations: [
-        {binding: 'foo', inspection: 'HasBinding'},
-    ]}
+  let(:expectations) {
+    [{'binding' => 'foo', 'inspection' => 'HasBinding'}]
   }
-  let(:json_with_not) {
-    {expectations: [
-        {binding: 'bar', inspection: 'Not:HasBinding'}
-    ]}
+  let(:expectations_with_not) {
+    [{'binding' => 'bar', 'inspection' => 'Not:HasBinding'}]
   }
-  let(:json_with_usage) {
-    {expectations: [
-        {binding: 'foo', inspection: 'HasUsage:bar'}
-    ]}
+  let(:expectations_with_usage) {
+    [{'binding' => 'foo', 'inspection' => 'HasUsage:bar'}]
   }
 
-  it { expect(ExpectationsRunner.expectations_to_terms(json)).to eq "[expectation('foo',inspection('HasBinding'))]" }
-  it { expect(ExpectationsRunner.expectations_to_terms(json_with_not)).to eq "[expectation('bar',not(inspection('HasBinding'))]" }
-  it { expect(ExpectationsRunner.expectations_to_terms(json_with_usage)).to eq "[expectation('foo',inspection('HasUsage',bar))]" }
+  it { expect(ExpectationsRunner.expectations_to_terms(expectations)).to eq "[expectation('foo',inspection('HasBinding'))]" }
+  it { expect(ExpectationsRunner.expectations_to_terms(expectations_with_not)).to eq "[expectation('bar',not(inspection('HasBinding')))]" }
+  it { expect(ExpectationsRunner.expectations_to_terms(expectations_with_usage)).to eq "[expectation('foo',inspection('HasUsage','bar'))]" }
 end
