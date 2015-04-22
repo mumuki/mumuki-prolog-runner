e82a74413f9caaf468ea06cc2d0b1a3c42c997bc
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-04-22 17:08:29 -0300

    Removing duplications and introducing negation

--- expectations/main.pl
+++ expectations/main.pl
@@ -13,18 +13,24 @@ run_expectations(Expectations, Results) :-
 
 run_expectation(Expectation, result(Expectation, Result)) :- eval_expectation(Expectation,Result).
 
-eval_expectation(expectation(Binding, inspection('HasBinding')), true) :-
-  usesPredicate(Binding, _), !.
-eval_expectation(expectation(Binding, inspection('HasForall')), true) :-
-  hasForall(Binding), !.
-eval_expectation(expectation(Binding, inspection('HasFindall')), true) :-
-  hasFindall(Binding), !.
-eval_expectation(expectation(Binding, inspection('HasNot')), true) :-
-  hasNot(Binding), !.
-eval_expectation(expectation(Binding, inspection('HasUsage', Other)), true) :-
-   usesPredicate(Binding, Other), !.
+eval_expectation(expectation(Binding, Inspection), true) :-
+  call_expectation(Binding, Inspection), !.
 eval_expectation(_, false).
 
+call_expectation(Binding, inspection('HasBinding')) :-
+  usesPredicate(Binding, _).
+call_expectation(Binding, inspection('HasForall')) :-
+  hasForall(Binding).
+call_expectation(Binding, inspection('HasFindall')) :-
+  hasFindall(Binding).
+call_expectation(Binding, inspection('HasNot')) :-
+  hasNot(Binding).
+call_expectation(Binding, inspection('HasUsage', Other)) :-
+   usesPredicate(Binding, Other).
+call_expectation(Binding, inspection('HasArity', Arity)) :-
+   hasArity(Binding, Arity).
+call_expectation(Binding, not(Inspection)) :-
+   \+ call_expectation(Binding, Inspection).
 
 write_results(Results) :-
   write('{"expectationResults":['),
