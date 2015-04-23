6e08305bd9d8b270135aef292a6ed021fec75f87
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-04-22 22:40:56 -0300

    Supporting multiple expectations and not

--- expectations/main.pl
+++ expectations/main.pl
@@ -6,16 +6,31 @@ main(_) :-
   consult('expectations/expectations.pl'),
   consult(Filename),
   run_expectations(Expectations, Results),
-  write_results(Results).
+  write_json(Results).
 
-write_results(Results) :-
+write_json(Results) :-
   write('{"expectationResults":['),
-  forall(member(Result, Results), write_result(Result)),
+  write_results(Results),
   write(']}').
 
+write_results([]).
+write_results([X1]) :-
+  write_result(X1).
+write_results([X1,X2|Xs]) :-
+  write_result(X1),
+  write(','),
+  write_results([X2|Xs]).
 
-write_result(result(expectation(Binding, inspection(Inspection)), Result)) :-
+write_result(result(expectation(Binding, Inspection), Result)) :-
+  swrite_inspection(Inspection, SInspection),
   writef('{"expectation":{"binding":"%w", "inspection":"%w"},"result":%w}',
-    [Binding, Inspection, Result]).
+    [Binding, SInspection, Result]).
 
+swrite_inspection(inspection(X), S) :-
+  swritef(S, "%w", [X]).
+swrite_inspection(inspection(X, Y), S) :-
+  swritef(S, "%w:%w", [X, Y]).
+swrite_inspection(not(Inspection), S2) :-
+  swrite_inspection(Inspection, S1),
+  swritef(S2, "Not:%w", [S1]).
 
