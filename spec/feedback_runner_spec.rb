e4558ee9b435a1e447b94eb6127cd6f40184cca9
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-06-10 02:59:38 -0300

    Adding tests for feedback_runner

--- spec/feedback_runner_spec.rb
+++ spec/feedback_runner_spec.rb
@@ -1,15 +1,31 @@
-require '../lib/feedback_runner'
 require 'ostruct'
+require_relative '../lib/feedback_runner'
 
 describe FeedbackRunner do
   let(:runner) { FeedbackRunner.new(nil) }
   let!(:feedback) { runner.run_feedback!(request, results) }
 
-  context 'when foo' do
-    let(:request) { OpenStruct.new }
-    let(:results) { OpenStruct.new }
+  context 'when wrong distinct operator' do
+    let(:request) { OpenStruct.new(content: 'foo(X) :- X != 2') }
+    let(:results) { OpenStruct.new(test_results: '') }
 
-    it { expect(feedback).to eq '' }
+    it { expect(feedback).to eq 'Recordá que el predicado infijo distinto en prolog se escribe así: \=' }
   end
+
+
+  context 'when wrong distinct operator' do
+    let(:request) { OpenStruct.new(content: '
+foo(X) :- bar(X).
+bar(3).
+foo(2).') }
+
+    let(:results) { OpenStruct.new(test_results: '
+Warning: /home/franco/tmp/mumuki-plunit-server/foo.pl:3:
+	Clauses of foo/1 are not together in the source-file
+% foo compiled 0.00 sec, 4 clauses') }
+
+    it { expect(feedback).to eq 'Recordá que es una buena práctica escribir toda las cláusulas de un mismo predicado juntas' }
+  end
+
 
 end
