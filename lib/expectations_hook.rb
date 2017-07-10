324140b26a40afe82bc6ae95831b956118ca1807
Franco Bulgarelli <franco@mumuki.org>
2017-07-10 10:39:32 -0300

    Using Mulang expectations hook

--- lib/expectations_hook.rb
+++ lib/expectations_hook.rb
@@ -1,63 +1,7 @@
-require 'json'
-require 'mumukit/inspection'
+class PrologExpectationsHook < Mumukit::Templates::MulangExpectationsHook
+  include_smells true
 
-class Mumukit::Inspection::PlainInspection
-  def to_term
-    "inspection('#{type}')"
-  end
-end
-
-class Mumukit::Inspection::TargetedInspection
-  def to_term
-    "inspection('#{type}',#{Integer(target)})" rescue "inspection('#{type}','#{target}')"
-  end
-end
-
-class Mumukit::Inspection::NegatedInspection
-  def to_term
-    "not(#{@inspection.to_term})"
-  end
-end
-
-class PrologExpectationsHook < Mumukit::Templates::FileHook
-  isolated true
-
-  def command_line(filename)
-    "swipl -f #{filename} --quiet -t main 2>&1"
-  end
-
-  def compile_file_content(request)
-    ExpectationsFile.new(request).render
-  end
-
-  def post_process_file(file, result, status)
-    JSON.parse(result)['expectationResults'] rescue []
-  end
-end
-
-class ExpectationsFile
-  def initialize(request)
-    @content = request.content
-    @expectation_terms = self.class.expectations_to_terms(request.expectations)
-  end
-
-  def self.expectations_to_terms(expectations)
-    '[' + expectations.map do |e|
-      "expectation('#{e['binding']}',#{inspection_to_term(e['inspection'])})"
-    end.join(',') + ']'
-  end
-
-  def self.inspection_to_term(s)
-    Mumukit::Inspection.parse(s).to_term
-  end
-
-  def render
-    ERB.new(File.read(template_path)).result(binding)
-  end
-
-  private
-
-  def template_path
-    File.expand_path '../main.pl.erb', __FILE__
+  def language
+    'Prolog'
   end
 end
